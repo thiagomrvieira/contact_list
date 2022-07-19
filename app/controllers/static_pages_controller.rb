c0bb58a754780a20a6221550f06ea6def26861bb
thiagomrvieira <thiagomrvieira@gmail.com>
2022-07-19 20:18:58 +0100

    Feat: Static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,10 @@ class StaticPagesController < ApplicationController
     def index
         @name = "Thiago Maciel"
     end
+
+    def sobre
+    end
+
+    def contato
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
   root "static_pages#index"
+
+  get "contato", to: "static_pages#contato"
+  get "sobre", to: "static_pages#sobre"
+
 end
