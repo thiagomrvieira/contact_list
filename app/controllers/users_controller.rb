39bb78b626f8689cb0634d4e59f40df755f96bc0
thiagomrvieira <thiagomrvieira@gmail.com>
2022-07-20 16:10:20 +0100

    Feat: Controller and Validations pt-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "User created!"
+            redirect_to root_path
+        else
+            flash[:user_errors] = @user.errors.full_messages
+            render :new
+        end
     end
+
+    private
+        def user_params
+            params.require(:user).permit(:email, :name, :password, :password_confirmation)
+        end
 end
